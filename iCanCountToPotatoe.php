39c9cb1dafabd6ae4f2c78c0ab88c89adf9e8ae4
Famamou <samsfanta@gmail.com>
2024-01-16 14:50:33 +0100

    CountToPotatoe

---
+++ iCanCountToPotatoe.php
@@ -0,0 +1,13 @@
+<?php
+
+  $limit= $_GET['number'];
+  $counter = 0;
+
+
+while ($counter <= $limit) {
+  echo $counter . "<br>";
+  $counter++;
+}
+
+
+?>
---
+++ iHardlyCanCountToPotatoe.php
@@ -0,0 +1,13 @@
+<?php
+
+  $limit= $_GET['number'];
+  $counter = 0;
+
+
+for ($counter=0 ; $counter<=$limit; $counter++) {
+  echo $counter . "<br>";
+  
+}
+
+
+?>
