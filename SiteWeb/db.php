4c274bc0067d972ae7993b2f92ca8684eaca9c83
Famamou <samsfanta@gmail.com>
2024-01-23 15:14:13 +0100

    modifications

--- SiteWeb/db.php
+++ SiteWeb/db.php
@@ -1,19 +1,21 @@
 <?php
-$user = "root";
-$pass = "";
-
+$user = "fanta";
+$pass = "famamouqwerty";
+$options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO ::FETCH_ASSOC
+];
 try {
     
-    $dbh = new PDO('mysql:host=localhost;dbname=site', $user, $pass);
+    $dbh = new PDO('mysql:host=localhost;dbname=site', $user, $pass,$options);
 }catch(PDOException $e){
     var_dump($e);
 }
 
-$query=$dbh->query('SELECT * FROM users');
+//$query=$dbh->query('SELECT * FROM users');
 
-$result = $query->fetchAll();
+//$result = $query->fetchAll();
 
-var_dump($result);
+//var_dump($result);
 
 ?>
 
--- SiteWeb/register.php
+++ SiteWeb/register.php
@@ -1,4 +1,8 @@
 <?php
+require_once("db.php");
+
+
+
 echo ("Bonjour, " .$_GET["firstname"]);
 echo ( "votre compte à bien été crée");
 echo ("<br>");
